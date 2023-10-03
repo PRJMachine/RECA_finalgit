1a8382583608181eb749e61d006b771f28271fec
Junho Hwang <iskasion@naver.com>
2023-10-03 17:25:07 +0900

    with login/signup & add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
     
-    from .views import main, chat
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+    # 블루프린트
+    from .views import main, chat, auth_views
     app.register_blueprint(main.bp)
     app.register_blueprint(chat.bp)
+    app.register_blueprint(auth_views.bp)
 
     return app
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class QuestionForm(FlaskForm):
     # subject = StringField('제목', validators=[DataRequired()])
     #요리재료 입력하는 칸. main.py - first.html 에서 이용할 계획
     content = TextAreaField('내용', validators=[DataRequired('먼저 요리재료를 입력해 주세요.')])
+
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[
+        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
+    email = EmailField('이메일', validators=[DataRequired(), Email()])
+
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired()])
