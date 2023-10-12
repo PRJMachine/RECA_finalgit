6c22efaccc0092bbbaeb5e17bc5cccb71cb11e74
jhhwang <iskasion@naver.com>
2023-10-12 10:32:00 +0900

    csrf key 만료에 따른 수정

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(__file__)
 # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://prj4:VMware1!@175.196.82.14:60306/pybo" #됨(온프렘 마스터db에 직결)
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://mha:mha@175.196.82.14:40306/pybo" #됨(온프렘 vIP에 연결)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "devdevdev"
+SECRET_KEY = "devdev231012"
 
 #인잇, 마이그레이션 하지말고 그냥 직접 만든 뒤 연결해서 쓰자
 # CREATE DATABASE pybo;
